9165f7ec421ea53e5ae60f6470a082b8531b7246
federico100hp <federico100hp@gmail.com>
2026-04-11 01:35:06 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useEffect, useCallback } from "react";
+import { useState, useMemo, useEffect } from "react";
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
 import { createClient } from "@supabase/supabase-js";
 
